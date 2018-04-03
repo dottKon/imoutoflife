cd309825f82bb056476fe5788165af2e11b1d26d
Konrad Hyzy <konradhyzy@gmail.com>
2018-04-03 20:34:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var countdownDate = new Date('Feb 16, 2018 16:00:00');
-var endOfDay = new Date('Feb 14, 2018 17:00:00')
+var endOfDay = new Date('Feb 14, 2018 16:00:00')
 var endWeekend = 60*60*24*4*1000 + 60*60*16*1000;
 var distance;
 
